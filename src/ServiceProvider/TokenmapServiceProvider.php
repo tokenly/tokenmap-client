864a7a59b7a1f44da098458bbd719bc5a547b4bd
Devon Weller <devon@tokenly.com>
2018-03-07 06:20:32 -0600

    fix service provider

--- src/ServiceProvider/TokenmapServiceProvider.php
+++ src/ServiceProvider/TokenmapServiceProvider.php
@@ -4,6 +4,7 @@ namespace Tokenly\TokenmapClient\ServiceProvider;
 
 use Illuminate\Support\ServiceProvider;
 use Tokenly\TokenmapClient\Console\GetQuoteCommand;
+use Tokenly\TokenmapClient\TokenmapClient;
 
 /*
  * TokenmapServiceProvider
@@ -27,12 +28,12 @@ class TokenmapServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('Tokenly\TokenmapClient\Client', function ($app) {
+        $this->app->bind(TokenmapClient::class, function ($app) {
             $cache_store = app('Tokenly\TokenmapClient\LaravelCacheStore\LaravelCacheStore');
 
             $tokenmap_connection_url = env('TOKENMAP_CONNECTION_URL', 'https://tokenmap.tokenly.com');
 
-            $tokenmap_client = new \Tokenly\TokenmapClient\Client($tokenmap_connection_url, $cache_store);
+            $tokenmap_client = new TokenmapClient($tokenmap_connection_url, $cache_store);
             return $tokenmap_client;
         });
     }
